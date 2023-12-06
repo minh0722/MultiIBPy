c649060d22c2bb6824cf9d7d72dfa022116456ea
Minh Nguyen <minh0722@yahoo.com>
2023-12-07 01:00:13 +0200

    move out the connect and disconnect immediately

--- main.py
+++ main.py
@@ -3,6 +3,12 @@ from ib_insync import *
 
 app = Flask(__name__)
 
+ib = IB()
+ib.connect('127.0.0.1', 7497, clientId=1, readonly=True)
+
+if(ib.isConnected):
+    print("Connected!")
+
 @app.route("/")
 def hello_world():
     response = "<p>Hello, World!</p>"
@@ -12,11 +18,7 @@ def hello_world():
 def test():
     response = "<p>Hello, test!</p>"
 
-    ib = IB()
-    ib.connect('127.0.0.1', 7496, clientId=1, readonly=True)
-
-    if(ib.isConnected):
-        response += "<p>Connected!</p>"
-        print("Connected!")
-
     return response
+
+ib.disconnect()
+print("Disconnected!")
